97ddb260fad830ea52645b5b3600e4d34615afc8
Kiran Chauhan <94677824+K1-ch@users.noreply.github.com>
2021-12-01 19:37:24 +0530

    Choice Program if-else
    Signed-off-by: Kiran Chauhan

---
+++ choice.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int i;
+clrscr();
+printf("Enter the num");
+scanf("%d",&i);
+if(i<=20)
+{
+printf("Hello");
+}
+else
+{
+printf("hii");
+}
+getch();
+}

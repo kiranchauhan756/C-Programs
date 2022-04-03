daf3bdc2695a51382c9396ff62743042762d5720
KIRAN CHAUHAN <kc7563330@gmail.com>
2022-04-03 21:58:00 +0530

    Structure with pointer example
    Signed-off-by: KIRAN CHAUHAN <kc7563330@gmail.com>

---
+++ structpoint.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+struct book{
+    int book_id;
+    char book_name[100];
+};
+void main(){
+    struct book *b1;
+    *b1.book_id=100;
+    strcpy(b1->book_name,"c programming");
+    printf("book_id=%d\t book_name=%s\n",b1->book_id,b1->book_name);
+}

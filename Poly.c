eabc2f590716cb6c0957563b2fd6a327f5dddc5e
Kiran Chauhan <94677824+K1-ch@users.noreply.github.com>
2022-04-25 14:38:21 +0530

    Update Poly.c

--- Poly.c
+++ Poly.c
@@ -11,27 +11,34 @@ int main(){
     int c,p,i;
     node *head=NULL;
     void insert_at_beg(node **,int,int);
-    void traverse(node *);
-    for(int i=0;i<=4;i++){
-        printf("Enter coefficient and power of term %d",i);
+    void traverse(node *,int);
+    for(int i=1;i<=4;i++){
+        printf("Enter coefficient and power of term %d\n",i);
         scanf("%d%d",&c,&p);
         insert_at_beg(&head,c,p);
+        
     }
-
+traverse(head,c);
     return 0;
 }
 
-void insert_at_beg(node **head,int coeff,int power){
+void insert_at_beg(node **head,int c,int p){
     node *temp;
     temp=(node *)malloc(sizeof(node));
     temp->coeff=c;
     temp->power=p;
     temp->next=*head;
+    *head=temp;
 }
 
-void traverse(node *head){
-    while(head->next!=NULL){
-        printf("%dX^%d+",head->coeff,head->power);
+
+void traverse(node *head,int coeff ){
+    while(head!=NULL){
+         printf("%dX^%d",head->coeff,head->power);
+        if(head->coeff>=0 ){
+            if(head->next!=NULL)
+            printf("+");
+    }
         head=head->next;
     }
 }

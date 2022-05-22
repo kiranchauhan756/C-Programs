7e2294ed27240e0ef567c1a838e358a91d285564
KiranChauhan <kc7563330@gmail.com>
2022-05-22 11:18:20 +0530

    //Insertion Sort
    Signed-off-by: KiranChauhan <kc7563330@gmail.com>

---
+++ InsertionSort.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void InsertEle(int a[],int n){
+    printf("Enter the elements\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+}
+
+void InsertionSort(int a[],int n){
+    int temp,i,j;
+    for(i=1;i<n;i++){
+        temp=a[i];
+        j=i-1;
+        while(j>=0 && a[j]>temp){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=temp;
+    }
+}
+
+void Print(int a[],int n){
+    printf("After Insertion Sort The  array is\n");
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+}
+
+void main(){
+int a[100],n;
+printf("Enter the number of elements\n");
+scanf("%d",&n);
+InsertEle(a,n);
+InsertionSort(a,n);
+Print(a,n);
+}
